7967704de72d8c226aeac0d193200966b39841b6
Josua Schmid <josua.schmid@renuo.ch>
2018-02-05 23:19:25 +0100

    Implement rebalancing

--- lib/renuo_blocks/chain.rb
+++ lib/renuo_blocks/chain.rb
@@ -13,9 +13,11 @@ module RenuoBlocks
       @blocks[block.number] ||= []
       @blocks[block.number].push(block)
 
-      rebalance if @blocks[block.number - 1].count > 1
+      # Rebalance if second to last block has more than one candidate
+      rebalance if !contested?(block.number) && contested?(block.number - 1)
     end
 
+    # For each positition in the chain the candidate positioned first is considered the valid one
     def blocks
       @blocks.map { |candidates| candidates&.first }
     end
@@ -31,13 +33,30 @@ module RenuoBlocks
       return false if Assessment.first?(block)                # early invalid if fake genesis
       return false unless Assessment.meets_difficulty?(block) # early invalid if difficulty not met
 
+      # Check if there's an existing parent
       @blocks[block.number - 1].any? do |previous_block|
         Assessment.valid_link?(previous_block, block)
       end
     end
 
+    # If a new last block comes in, we realign the first blocks to build the longest chain
     def rebalance
+      winning_block = @blocks.last.first
+      parent_position = @blocks.count - 2
 
+      while contested?(parent_position)
+        @blocks[parent_position].sort_by! do |previous_block|
+          Assessment.valid_link?(previous_block, winning_block) ? 0 : 1
+        end
+
+        winning_block = @blocks[parent_position].first
+        parent_position -= 1
+      end
+    end
+
+    # Contested evaluates to true if there blocks are competing for the same position in the blockchain
+    def contested?(block_number)
+      @blocks[block_number].count > 1
     end
   end
 end
