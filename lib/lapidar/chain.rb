20be91bc017a1f7a857162a1e2a089f88b79ab5b
Josua Schmid <josua.schmid@renuo.ch>
2019-09-09 21:32:31 +0200

    Rename instance variable

--- lib/lapidar/chain.rb
+++ lib/lapidar/chain.rb
@@ -1,17 +1,17 @@
 module Lapidar
   class Chain
     def initialize
-      @blocks = []
+      @block_stacks = []
     end
 
     # TODO: Queue up future blocks for later use
     # TODO: Check for duplicates and dont add them to the chains
     def add(block)
-      raise "future block?" if block.number > @blocks.count
+      raise "future block?" if block.number > @block_stacks.count
       raise "invalid block" unless valid?(block)
 
-      @blocks[block.number] ||= []
-      @blocks[block.number].push(block) unless @blocks[block.number].map(&:hash).include?(block.hash)
+      @block_stacks[block.number] ||= []
+      @block_stacks[block.number].push(block) unless @block_stacks[block.number].map(&:hash).include?(block.hash)
 
       # Rebalance if second to last block has more than one candidate
       rebalance if !contested?(block.number) && contested?(block.number - 1)
@@ -19,12 +19,12 @@ module Lapidar
 
     # For each positition in the chain the candidate positioned first is considered the valid one
     def blocks
-      @blocks.map { |candidates| candidates&.first }
+      @block_stacks.map { |candidates| candidates&.first }
     end
 
     def to_colorful_string(depth = 0)
       [*0..depth].map do |level|
-        @blocks.map do |block_stack|
+        @block_stacks.map do |block_stack|
           if block_stack[level]
             number_display = block_stack[level].number.to_s
             if defined? Paint
@@ -48,30 +48,30 @@ module Lapidar
       return false unless Assessment.meets_difficulty?(block) # early invalid if difficulty not met
 
       # Check if there's an existing parent
-      @blocks[block.number - 1].any? do |previous_block|
+      @block_stacks[block.number - 1].any? do |previous_block|
         Assessment.valid_link?(previous_block, block)
       end
     end
 
     # If a new last block comes in, we realign the first blocks to build the longest chain
     def rebalance
-      winning_block = @blocks.last.first
-      parent_position = @blocks.count - 2
+      winning_block = @block_stacks.last.first
+      parent_position = @block_stacks.count - 2
 
       while contested?(parent_position)
         # TODO: Is there's a smarter way to persistently select a winner than sorting the competition
-        @blocks[parent_position].sort_by! do |previous_block|
+        @block_stacks[parent_position].sort_by! do |previous_block|
           Assessment.valid_link?(previous_block, winning_block) ? 0 : 1
         end
 
-        winning_block = @blocks[parent_position].first
+        winning_block = @block_stacks[parent_position].first
         parent_position -= 1
       end
     end
 
     # Contested evaluates to true if there blocks are competing for the same position in the blockchain
     def contested?(block_number)
-      @blocks[block_number].count > 1
+      @block_stacks[block_number].count > 1
     end
   end
 end
